8d8bc3c959ced0469dd3879c9db33c7bca08ae03
Ian Norris <IanNorris@users.noreply.github.com>
2023-07-13 22:48:12 +0100

    Fonts unsigned char

--- fonts/Jura.h
+++ fonts/Jura.h
@@ -1,2 +1,2 @@
-extern const char Jura_fnt_data[];
+extern const unsigned char Jura_fnt_data[];
 extern const unsigned int Jura_fnt_size;
--- fonts/Jura_0.h
+++ fonts/Jura_0.h
@@ -1,2 +1,2 @@
-extern const char Jura_0_tga_data[];
+extern const unsigned char Jura_0_tga_data[];
 extern const unsigned int Jura_0_tga_size;
