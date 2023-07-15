62ee9e10cae86c180869ac1b485c109b288c1c14
Ian Norris <IanNorris@users.noreply.github.com>
2023-07-15 03:05:09 +0100

    Kernel copied to boot image, and bytes dumped in the bootloader

--- src/bootloader/entry.c
+++ src/bootloader/entry.c
@@ -20,6 +20,25 @@ CEfiStatus efi_main(CEfiHandle imageHandle, CEfiSystemTable* systemTable)
 	CEfiGuid loadedImageServiceGuid = C_EFI_LOADED_IMAGE_PROTOCOL_GUID;
 	CEfiLoadedImageProtocol* loadedImageService;
 	ERROR_CHECK(bootServices->handle_protocol(imageHandle , &loadedImageServiceGuid, (void**)&loadedImageService), L"LI service");
+
+	CEfiChar16 buffer[16];
+	
+	Print(systemTable, L"\r\n\r\n");
+
+	for (int byte = 0; byte < 128; byte += 16)
+	{
+		for (int byteOffset = 0; byteOffset < 16; byteOffset++)
+		{
+			witoabuf(buffer, ((unsigned char*)loadedImageService->image_base)[byte + byteOffset], 16);
+
+			Print(systemTable, buffer);
+			Print(systemTable, L" ");
+		}
+		Print(systemTable, L"\r\n");
+	}
+
+	Print(systemTable, L"\r\n");
+		
 	
 	Print(systemTable, L"2");
 
