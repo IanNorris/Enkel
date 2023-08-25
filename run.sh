59765c5096c09c149736ad6c406ba5fcccf6f15b
Ian Norris <IanNorris@users.noreply.github.com>
2023-08-26 00:47:36 +0100

    Fix warning about raw format

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 echo Starting qemu
 
-qemu-system-x86_64 ./boot_iso/enkel.iso -bios ./boot_iso/ovmf_bios.bin -m 2G -vga virtio -display gtk,zoom-to-fit=off -d int,cpu_reset -D ./qemu_log.log -gdb tcp:localhost:5000
+qemu-system-x86_64 -drive format=raw,media=cdrom,readonly=on,file=./boot_iso/enkel.iso -bios ./boot_iso/ovmf_bios.bin -m 2G -vga virtio -display gtk,zoom-to-fit=off -d int,cpu_reset -D ./qemu_log.log -gdb tcp:localhost:5000
