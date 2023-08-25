207975316bf46e04323228df919b5e6852ceb957
Ian Norris <IanNorris@users.noreply.github.com>
2023-08-25 02:27:03 +0100

    Improved debugging experience

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+echo Starting qemu
+
 qemu-system-x86_64 ./boot_iso/enkel.iso -bios ./boot_iso/ovmf_bios.bin -m 2G -vga virtio -display gtk,zoom-to-fit=off -d int,cpu_reset -D ./qemu_log.log -gdb tcp:localhost:5000
