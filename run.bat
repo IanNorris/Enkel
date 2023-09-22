a64dee33b002444e27e0e69053c0f3d6bec45c1a
Ian Norris <IanNorris@users.noreply.github.com>
2023-09-22 01:41:48 +0100

    Added Windows run script

---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+
+start /B "" "C:\Program Files\qemu\qemu-system-x86_64w.exe" -drive format=raw,media=cdrom,readonly=on,file=./boot_iso/enkel.iso -bios ./boot_iso/ovmf_bios.bin -serial mon:stdio -m 8G -smp sockets=2,cores=4 -vga virtio -display gtk,zoom-to-fit=off -d int,cpu_reset -D ./qemu_log.log -gdb tcp:localhost:5000
