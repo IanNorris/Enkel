37fe7e2e9eae4791f19cb394a1e25b69b1794fa9
Ian Norris <IanNorris@users.noreply.github.com>
2023-12-20 03:30:13 +0000

    Volatile friendly memset & memcpy

--- inc/memory/memory.h
+++ inc/memory/memory.h
@@ -3,7 +3,9 @@
 #include "common/types.h"
 
 void* memcpy(void* dest, const void* src, size_t n);
+volatile void* memcpy(volatile void* dest, const void* src, size_t n);
 void* memset(void* s, uint8_t c, size_t n);
+volatile void* memset(volatile void* s, uint8_t c, size_t n);
 void* memset32(void* s, uint32_t value, size_t n);
 void* memset64(void* s, uint64_t value, size_t n);
 void* memmove(void* dest, const void* src, size_t n);
--- src/kernel/memory/memory.cpp
+++ src/kernel/memory/memory.cpp
@@ -18,9 +18,33 @@ void* memcpy(void* dest, const void* src, size_t n)
     return dest;
 }
 
+volatile void* memcpy(volatile void* dest, const void* src, size_t n) 
+{
+    volatile uint8_t* pdest = (volatile uint8_t*)dest;
+    const uint8_t* psrc = (const uint8_t*)src;
+
+    for (size_t i = 0; i < n; i++) {
+        pdest[i] = psrc[i];
+    }
+
+    return dest;
+}
+
 void* memset(void* s, uint8_t value, size_t n)
 {
     uint8_t* p = (uint8_t*)s;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        p[i] = value;
+    }
+
+    return s;
+}
+
+volatile void* memset(volatile void* s, uint8_t value, size_t n)
+{
+    volatile uint8_t* p = (volatile uint8_t*)s;
 
     for (size_t i = 0; i < n; i++)
     {
